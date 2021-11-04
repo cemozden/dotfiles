c2c64cafd015d302be08640642e5d01159cff8b6
Cem Özden <cemozden.tr@gmail.com>
2021-11-04 19:26:16 +0100

    Adding default editor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -x PATH $PATH . /home/cemozden/bin/ /home/cemozden/.local/share/gem/ruby/3.0.0/bin
+    set -x PATH $PATH . /home/ozdenc/bin/ /home/ozdenc/.local/share/gem/ruby/3.0.0/bin
+    set -x EDITOR "nvim"
 end
