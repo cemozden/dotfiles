0eda991f4698f9dec905f16ed3de77d8234f1cce
Cem Özden <cemozden.tr@gmail.com>
2022-01-16 21:30:59 +0300

    Changes in dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -x PATH $PATH . /home/ozdenc/bin/ /home/ozdenc/.local/share/gem/ruby/3.0.0/bin
+    set -x PATH $PATH . ~/bin/ ~/.local/share/gem/ruby/3.0.0/bin
     set -x EDITOR "nvim"
     set -x TERM "xterm-256color"
     alias t "tmux attach-session -d"
