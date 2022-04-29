8d8f2ebdaf5e25c20a00e7666748cbf99b5463f9
Cem Özden <cemozden.tr@gmail.com>
2022-04-29 23:04:29 +0200

    Updating dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x PATH $PATH . ~/bin/ ~/.local/share/gem/ruby/3.0.0/bin /home/linuxbrew/.linuxbrew/bin/
-    set -x EDITOR "vim"
+    set -x EDITOR "nvim"
+    set -x GTK_MODULES "canberra-gtk-module:canberra-gtk-module"
     set -x TERM "xterm-256color"
     alias t "tmux attach-session -d"
     alias py "python"
